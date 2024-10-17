d1b8c4e5ed5291e889e31f6685ef67db19b5e1be
Joao Rangel <joaohcrangel@gmail.com>
2024-10-17 18:32:34 -0300

    cmd start

--- actions/start.action.ts
+++ actions/start.action.ts
@@ -1,10 +1,11 @@
-import { Socket } from 'net';
 import { AbstractAction } from './abstract.action';
 import chalk = require('chalk');
 import { spawn, SpawnOptions } from 'child_process';
 import { join } from 'path';
 import { EMOJIS } from '../lib/ui';
 import { existsSync } from 'fs';
+import { getRootPath } from '../lib/utils/get-root-path';
+import * as net from 'net';
 
 type Command = {
   command: string;
@@ -16,71 +17,39 @@ type Command = {
 export class StartAction extends AbstractAction {
   public async handle() {
     console.info(chalk.white(`Starting HedHog ${EMOJIS.HEDGEHOG}...`));
-    this.runConcurrently([
-      {
-        command: 'npm run dev',
-        cwd: join(process.cwd(), 'backend'),
-        name: 'API',
-      },
-      {
-        command: 'npm run dev',
-        cwd: join(process.cwd(), 'admin'),
-        name: 'Admin',
-      },
-    ]);
+    const rootPath = await getRootPath();
+    await this.startProcess(
+      'API',
+      'npm',
+      ['run', 'dev'],
+      join(rootPath, 'backend'),
+    );
+    await this.startProcess(
+      'ADM',
+      'npm',
+      ['run', 'dev'],
+      join(rootPath, 'admin'),
+    );
     await this.waitForPorts();
   }
 
-  async runConcurrently(commands: Command[]) {
-    const colors = [
-      chalk.blue,
-      chalk.green,
-      chalk.red,
-      chalk.yellow,
-      chalk.magenta,
-      chalk.cyan,
-    ];
-    commands.forEach(({ command, cwd, name, options = {} }, index) => {
-      try {
-        options.cwd = cwd;
-        options.shell = true;
-        let file = '/bin/sh';
-        let args = ['-c', command];
-        if (process.platform === 'win32') {
-          file = 'cmd.exe';
-          args = ['/s', '/c', `"${command}"`];
-          options.windowsVerbatimArguments = true;
-        }
-
-        if (existsSync(options.cwd)) {
-          const child_process = spawn(file, args, options);
-
-          if (child_process.stdout) {
-            child_process.stdout.on('data', (data) => {
-              console.info(colors[index % colors.length](`[${name}] ${data}`));
-            });
-          }
-
-          if (child_process.stderr) {
-            child_process.stderr.on('data', (data) => {
-              console.error(chalk.red(`[${name} ERROR] ${data}`));
-            });
-          }
-
-          child_process.on('close', (code) => {
-            console.info(`[${name}] exited with code ${code}`);
-          });
-        } else {
-          console.error(
-            chalk.red(
-              `[${name} ERROR] Directory ${options.cwd} does not exist`,
-            ),
-          );
-        }
-      } catch (error) {
-        console.error(chalk.red(`[${name} ERROR] ${error}`));
-      }
+  async startProcess(id: string, bin: string, args: string[], cwd: string) {
+    console.log(`Starting ${bin} ${args.join(' ')} in ${cwd}`);
+    const childProcess = spawn(bin, args, {
+      cwd,
+      stdio: 'inherit',
+      shell: true,
     });
+
+    childProcess.stderr?.on('data', (data) => {
+      console.error(id, data.toString());
+    });
+
+    childProcess.stdout?.on('data', (data) => {
+      console.log(id, data.toString());
+    });
+
+    return childProcess;
   }
 
   async waitForPorts() {
@@ -88,7 +57,7 @@ export class StartAction extends AbstractAction {
     let frontendReady = false;
 
     while (!apiReady || !frontendReady) {
-      console.info('Waiting for ports to be available...');
+      console.info('Waiting for ports...');
       apiReady = await this.checkPort(3000);
       frontendReady = await this.checkPort(3100);
 
@@ -102,30 +71,29 @@ export class StartAction extends AbstractAction {
 
   complete() {
     console.clear();
-    console.info(chalk.white('===================================='));
+    console.info();
     console.info(chalk.green(`${EMOJIS.HEDGEHOG} HedHog is ready!`));
+    console.info();
     console.info(chalk.blue('API is running on http://localhost:3000'));
     console.info(chalk.red('Admin is running on http://localhost:3100'));
-    console.clear();
+    console.info();
   }
 
   async checkPort(port: number, host = 'localhost') {
-    return new Promise<boolean>((resolve) => {
-      const socket = new Socket();
-      socket.setTimeout(1000);
-      socket
-        .on('connect', () => {
-          socket.destroy();
+    return new Promise<boolean>((resolve, reject) => {
+      try {
+        const client = net.connect({ port, host }, () => {
+          client.end();
           resolve(true);
-        })
-        .on('timeout', () => {
-          socket.destroy();
+        });
+
+        client.on('error', () => {
           resolve(false);
-        })
-        .on('error', () => {
-          resolve(false);
-        })
-        .connect(port, host);
+        });
+      } catch (error) {
+        console.error('Error:', error);
+        reject(error);
+      }
     });
   }
 }
