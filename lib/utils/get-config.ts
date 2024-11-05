1f0715ed7f58aab9f56862d987d92726570b0300
Gabriel Lima <gabriellpbiel@hotmail.com>
2024-11-05 08:53:15 -0300

    fix: add error handling for missing configuration directory and file

--- lib/utils/get-config.ts
+++ lib/utils/get-config.ts
@@ -2,10 +2,20 @@ import { readFile } from 'fs/promises';
 import { homedir } from 'os';
 import { join } from 'path';
 import { parse } from 'yaml';
+import { access } from 'fs/promises';
+import chalk = require('chalk');
 
 export const getConfig = async (path: string) => {
   const dirPath = join(homedir(), '.hedhog');
   const configPath = join(dirPath, 'config.yaml');
+
+  try {
+    await access(dirPath);
+    await access(configPath);
+  } catch (err) {
+    chalk.red('Configuration file not found');
+    return;
+  }
 
   const content = parse(await readFile(configPath, 'utf-8'));
 
