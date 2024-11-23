090a670440112575b6f01823d47f842c9a517384
Joao Rangel <joaohcrangel@gmail.com>
2024-11-23 14:12:07 -0300

    🚀 Auto commit before deploy

--- lib/runners/runner.ts
+++ lib/runners/runner.ts
@@ -6,4 +6,5 @@ export enum Runner {
   NPX,
   DOCKER,
   NESTJS,
+  GIT,
 }
---
+++ scripts/deploy.ts
@@ -0,0 +1,44 @@
+import { execSync } from 'child_process';
+
+function runCommand(command: string) {
+  try {
+    return execSync(command, { stdio: 'inherit' });
+  } catch (error) {
+    console.error(`Error executing command: ${command}`);
+    process.exit(1);
+  }
+}
+
+function main() {
+  try {
+    const gitStatus = execSync('git status -s').toString().trim();
+
+    console.log({ gitStatus });
+
+    if (gitStatus) {
+      runCommand('git add .');
+      runCommand('git commit -m "🚀 Auto commit before deploy"');
+    }
+
+    const gitLog = execSync('git log origin/master..master').toString().trim();
+
+    console.log({ gitLog });
+
+    if (gitLog) {
+      runCommand('git pull origin master');
+      runCommand('git push origin master');
+    }
+
+    runCommand('git checkout production');
+    runCommand('git merge master');
+    runCommand('git push origin production');
+    runCommand('git checkout master');
+
+    console.log('✅ Deploy realizado com sucesso!');
+  } catch (error) {
+    console.error('An error occurred during deployment:', error);
+    process.exit(1);
+  }
+}
+
+main();
