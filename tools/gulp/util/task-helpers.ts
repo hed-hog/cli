86ae3ce66595844669ccae1a9d41b65db939b424
saulotarsobc <saulotarsobc@gmail.com>
2024-12-17 12:56:19 -0300

    Update .gitignore and build configuration
    • Added 'types/**/*.js' and 'types/**/*.d.ts' to .gitignore
    • Included 'types' directory in sources array for build process
    • Enhanced documentation for utility functions in task-helpers.ts
    
    These changes improve the project structure by including the 'types' directory in the build process and ignoring generated files. The added documentation enhances code readability and maintainability.

--- tools/gulp/util/task-helpers.ts
+++ tools/gulp/util/task-helpers.ts
@@ -1,14 +1,32 @@
 import { readdirSync, statSync } from 'fs';
 import { join } from 'node:path';
 
-function isDirectory(path: string) {
+/**
+ * Checks if the provided path is a directory.
+ *
+ * @param path - The path to check.
+ * @returns { boolean } - True if the path is a directory, false otherwise.
+ */
+function isDirectory(path: string): boolean {
   return statSync(path).isDirectory();
 }
 
+/**
+ * Returns an array of all folder names in the provided directory.
+ *
+ * @param dir - The directory from which to retrieve the folder names.
+ * @returns An array of strings representing the folder names in the directory.
+ */
 export function getFolders(dir: string) {
   return readdirSync(dir).filter((file) => isDirectory(join(dir, file)));
 }
 
+/**
+ * Returns an array of all directories in the provided directory.
+ *
+ * @param base - The directory from which to retrieve the directories.
+ * @returns An array of strings representing the directories in the directory.
+ */
 export function getDirs(base: string) {
   return getFolders(base).map((path) => `${base}/${path}`);
 }
